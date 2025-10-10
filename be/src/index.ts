ba1be10b780e65d2db9485667193318e810757fb
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-10-10 22:42:07 +0530

    did something

--- be/src/index.ts
+++ be/src/index.ts
@@ -9,7 +9,7 @@ async function main() {
 
     const stream = await client.chat.completions.create({
       model: "sonar",
-      messages: [{ role: "user", content: ""} ,
+      messages: [{ role: "user", content: "For all designs I ask you to make, have them be beautiful, not cookie cutter. Make webpages that are fully featured and worthy for production.\n\nBy default, this template supports JSX syntax with Tailwind CSS classes, React hooks, and Lucide React for icons. Do not install other packages for UI themes, icons, etc unless absolutely necessary or I request them.\n\nUse icons from lucide-react for logos.\n\n"}  ,
         { role: "user", content: ""} ,
         { role: "user", content: "Create a TODO application"
        }],
